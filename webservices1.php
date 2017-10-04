ea2dbd20f620434de9f8526303e3bf528da5f2db
1986webdeveloper <1986webdeveloper@gmail.com>
2017-10-04 15:02:43 +0530

    upload changes

---
+++ webservices1.php
@@ -0,0 +1,131 @@
+<?php
+define('ABSPATH', dirname(__FILE__) . '/');
+include("conn.php");
+DEFINE('IMAGE_URL',"http://192.168.1.1/lottery/admin/gallery/");
+DEFINE('LOTTORY_URL',"http://192.168.1.1/lottery/admin/images/");
+
+//DEFINE('UPLOAD_URL',"admin/gallery/");
+define('UPLOAD_URL', $_SERVER['DOCUMENT_ROOT'] . '/lottery/admin/gallery/');
+
+class webservice {
+    private $connection;
+//login,register,update_profile,verify_otp
+    public function webservice($requestArr) {
+        global $con;
+        $this->connection = $con;
+        $this->action = $requestArr['action'];
+        switch ($this->action) {
+           
+            case 'push_android':
+                $this->push_android($requestArr);
+                break;
+            case 'push_ios':
+                $this->push_ios($requestArr);
+                break;
+        
+
+        }
+    }
+   
+
+    function push_android($requestArr){
+        $title = mysqli_real_escape_string($this->connection,$requestArr['title']);
+         $message = mysqli_real_escape_string($this->connection,$requestArr['message']);
+         $symbol_id = mysqli_real_escape_string($this->connection,$requestArr['symbol_id']);
+         $symbol_title = mysqli_real_escape_string($this->connection,$requestArr['symbol_title']);
+         $timeframe = mysqli_real_escape_string($this->connection,$requestArr['timeframe']);
+          $symbolmonth = mysqli_real_escape_string($this->connection,$requestArr['symbolmonth']);
+         
+         $device_token = mysqli_real_escape_string($this->connection,$requestArr['device_token']);
+         // echo file_get_contents('php://input');exit;
+           
+            
+        require_once 'Firebase.php';        
+        $mPushNotification = array();
+        $mPushNotification['data']['title'] = $title;
+        $mPushNotification['data']['message'] = $message;
+        $mPushNotification['data']['symbol_id'] = $symbol_id;
+        $mPushNotification['data']['symbol_title'] = $symbol_title;
+        $mPushNotification['data']['timeframe'] = $timeframe;
+        $mPushNotification['data']['symbolmonth'] = $symbolmonth;
+         
+        $devicetoken = array($device_token);
+         //creating firebase class object 
+        $firebase = new Firebase(); 
+         
+         //sending push notification and displaying result 
+        header('Content-type: application/json');
+        echo $firebase->send($devicetoken, $mPushNotification);
+
+    }
+    function push_ios($requestArr){{
+
+         $title = mysqli_real_escape_string($this->connection,$requestArr['title']);
+         $message = mysqli_real_escape_string($this->connection,$requestArr['message']);
+         $symbol_id = mysqli_real_escape_string($this->connection,$requestArr['symbol_id']);
+         $symbol_title = mysqli_real_escape_string($this->connection,$requestArr['symbol_title']);
+         $timeframe = mysqli_real_escape_string($this->connection,$requestArr['timeframe']);
+          $symbolmonth = mysqli_real_escape_string($this->connection,$requestArr['symbolmonth']);
+         
+         $deviceToken = mysqli_real_escape_string($this->connection,$requestArr['device_token']);
+
+        $passphrase = 'agc123';
+        $ctx = stream_context_create();
+        // ck.pem is your certificate file
+        stream_context_set_option($ctx, 'ssl', 'local_cert', $_SERVER['DOCUMENT_ROOT'].'/'.basename(__DIR__).'/apns-dev-cert.pem');
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+        // Open a connection to the APNS server
+        $fp = stream_socket_client(
+            'ssl://gateway.sandbox.push.apple.com:2195', $err,
+            $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+        if (!$fp)
+            exit("Failed to connect: $err $errstr" . PHP_EOL);
+        // Create the payload body
+
+        $body['aps'] = array(
+            'alert' => array(
+                'title' => $title,
+                'body' => $message,
+             ),
+            'sound' => 'default'
+        );
+
+        $body['data']['title'] = $title;
+        $body['data']['message'] = $message;
+        $body['data']['symbol_id'] = $symbol_id;
+        $body['data']['symbol_title'] = $symbol_title;
+        $body['data']['timeframe'] = $timeframe;
+        $body['data']['symbolmonth'] = $symbolmonth;
+        // Encode the payload as JSON
+        $payload = json_encode($body);
+        // Build the binary notification
+        $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+        // Send it to the server
+        $result = fwrite($fp, $msg, strlen($msg));
+        
+        // Close the connection to the server
+        fclose($fp);
+        if (!$result)
+            return 'Message not delivered' . PHP_EOL;
+        else{
+            header('Content-type: application/json');
+            echo json_encode($body); exit;
+        
+        }
+    }
+}
+}
+
+if (isset($_REQUEST['action'])) {
+
+    $webservice = new webservice($_REQUEST);
+} else {
+    $jsonOutput['meta']['status'] = 'error';
+    $jsonOutput['meta']['code'] = '400';
+    $jsonOutput['meta']['message'] = 'Please enter proper action.';
+    $jsonOutput['data'] = '';
+    header('Content-type: application/json');
+    echo json_encode($jsonOutput); 
+}
+
+?>
---
+++ welcome.php
@@ -0,0 +1,145 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Expires" content="-1">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<script type="text/javascript" src="js/jquery-2.2.0.min.js"></script>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+<style type="text/css">
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+body{
+font-family: "Roboto", sans-serif;
+ font-size: 12pt;
+ color: #111111;
+ background: #f1fcff;
+ -webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+}
+div.hr{
+background-color: #111111;
+border: 0px;
+height: 1px;
+margin: 0px;
+width: 600px;
+overflow: hidden;
+}
+/* Header */
+.header{
+background: #fafbfc;
+color: #070707;
+position: fixed;
+left:0;
+-moz-box-sizing: border-box;
+box-sizing: border-box;
+width: 100%;
+z-index: 100;
+max-width:100%;
+}
+.header{
+top: 0;
+border-bottom:1px solid #e7e6e7;
+text-align: center;
+height: 80px;
+}
+.welcome_page {
+width: auto;
+margin: 0 auto;
+}
+.welcome_page_div {
+position: relative;
+ z-index: 1;
+ background: #FFFFFF;
+ width: 50%;
+ width: 100%;
+ max-width: 480px;
+ padding: 1 0 10 0;
+ margin-top: 180px;
+ text-align: center;
+ border-radius: 8px;
+ box-shadow: 0 0 4px rgba(0, 0, 0, 0.2), 0 0 4px rgba(0, 0, 0, 0.24);
+}
+.logo {
+float:left;
+margin:15px 15px;
+}
+.header-right-text {
+text-align:right;
+float:right;
+line-height:80px;
+padding-right:15px;
+font-size:14px;
+color:#000;
+font-weight: 600;
+}
+.footer {
+text-align:center;
+font-size:14px;
+color:#000;
+font-weight: 600;
+}
+.footer-margin {
+margin-top:99px;
+}
+@media (max-width:640px) {
+.header-right-text {
+ float: right;
+ font-size: 14px;
+ line-height: 19px;
+ padding: 20px 15px;
+ text-align: right;
+ width: 35%;
+}
+}
+</style>
+</head>
+<body>
+<div class="header">
+<h1 class="logo"><img src="images/new_logo.png" alt="Logo"/></h1>
+<span class="header-right-text">Blue Box Firewall Secured Network</span>
+</div>
+<center>
+<div class="welcome_page">
+<div class="welcome_page_div">
+<img
+src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIwAAACECAIAAADN8VQEAAAENUlEQVR42u3cvWtUQ
+RjF4bOYxPgVRGxip2hhIdFGsNBKK0s77bTQSmy1srG2kpQ2BvwnFCxUBBGxEgkWNqIiCioqEePCiyFssnfn686878z5VckGli
+wPhM3euWewuroKprsBkfRHJAMRyUBEMhCRDEQkAxHJQEQyEJEMRCQDEclARDIQkQxEJAMRyUBEMhCR0vfpx7vt03M
+7ZnanekIiJe7V+weLTy/Pzx28dvLe3Na9SZ6TSCkToV9/vg+/3r/naConIiVrvZCUyolIadooJCVxIlKCxglJ8U5Eiq1bSIp0IlJULkJS
+jBORwnMXkoKdiBSYr5AU5kSkkMKEpAAnInkXIyT5OhHJr3ghycuJSB6lEpLcnYjkWlohydGJSE71ISS5OBFpcv0JSROdiDShv
+oWkbicidZVHSOpwItLYcgpJ45yItHn5hYYNMDh35PrZw1dHHyfSxkoJnTpw/sKxW9NbZkd/RKSRtAmBSCMpFAKR1qdTCE
+RaS60QiCRpFgKRoF4IRNIvhMaRTAihZSQrQmgWyZAQkiM9XL77+uOTi8dvz07tzPn6vbIlhLRIQ6H7L2+u/P29MH/myolFn
+U7mhJAQaU1IvtXpZFEIqZBGhCRtTkaFkARpUyFJj5NdIcQjdQhJGpxMCyESaaKQVNbJuhBikByFpFJOFQghGMlLSMrvVIcQ
+wpAChKScTtUIIQApWEjK41STEHyRIoWkvp0qE4IXUhIhqT+n+oTgjpRQSOrDqUohOCIlF5LSOtUqBBeknoSkVE4VC2EiUq9
+CUrxT3ULoRsogJMU4VS+EDqRsQlKYUwtCGIf06O3S0osb2YQkX6dGhDAOafnz8zuPL3399SHn64ePUztC6Phzp9mpKSF0v
+3HQ6dSaECa+Bdfm1KAQXP6Z1ePUphAcPxbS4NSsENw/YC3r9ObTs2aF4HWpopTT/K5DX36+b1YIvhf9SjllTpUQAi6fV++k
+TQhhB1EqdlIohOAjXVU66RRCzOHIypzUCiHymHE1TpqFEH9gvwIn5UJIcuuLaSf9Qkh1E5lRJxNCSHg7pjknK0JIe2OzISdDQ
+kg+EWDCyZYQ+hjbUO5kTgg9LaKodbIohP5maxQ6GRVCr9tCqpzsCqHvASglTqaFkGGlq7iTdSHkmVIr6FSBELLt3RVxqkMI
+OUcJMztVI4TMy5HZnGoSQv55zwxOlQmhyAZrr071CaHUUG5PTlUKoeCacXKnWoVQdnI6oVPFQii+C57EqW4hFEdCtFP1
+QtCAhAinFoSgBAlBTo0IQQ8SPJ3aEYIqJDg7NSUEbUhwcGpNCAqR0OnUoBB0ImGMU5tCUIuEDU7NCkEzEtY5tSwE5Uj47
+7Sw73SzQtCPNOznyreZqW1bBlOlf5FiGUBiRDIQkQxEJAMRyUBEMhCRDEQkAxHJQEQyEJEMRCQDEclARDIQkQz0D8p98
+0Th0krqAAAAAElFTkSuQmCC" onclick="return open_default_browserpage();"/>
+</p>
+You're logged-in as <br>
+Restart your browser to clear out the browser cache.
+</center>
+</div>
+</div>
+<footer class="footer footer-margin">Blue Box Firewall © 2017 all rights reserved </footer>
+</body>
+</html>
+
+<script type="text/javascript">
+
+
+function open_default_browserpage()
+{
+
+  browsercheck.performClick();
+  
+}
+
+				
+</script>
